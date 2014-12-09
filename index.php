4b133fc3ddd30ed3c1356596c092a541b771922f
eyobdejene <eyob_westerink@hotmail.com>
2014-12-09 09:38:57 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,160 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>admin</title>
+</head>
+
+<body>
+
+<!-- Start wrapper !-->
+
+
+<div  id="wrapper">
+
+<!-- Start menu !-->
+	<div id="menu">
+
+			<!--Start logo !-->
+			<div class="logo">
+				<img alt="logo" src="" />
+			</div>
+
+			<!--Start navigatie !-->
+			<div class="nav">
+
+				<ul> 
+					<li>Nieuws</li>
+					<li>Agenda</li>
+					<li>Plein</li>
+					<li>initiatieven</li>
+					<li>Discussies</li>
+
+				</ul>
+			</div>
+
+			
+			<!--Start profiel img !-->
+			<div class="profile_img">
+			 	<img alt="profile_foto"src="" /> 
+			</div>
+
+
+			<!--Start mentions !-->
+			<div class="nofications">
+
+					<div class="notification_icon">
+						<img alt="notification_icon"src=""/>
+					</div>
+
+					<div class="user_name">
+						<img alt="user_name"src=""/>
+					</div>	
+
+					<div class="notification_number">
+						<p>1</p> 
+					</div>
+			</div>	
+
+	</div>
+
+
+	<!--Start banner !-->
+	<div id="banner">
+
+		<img alt="banner" src="" /> 
+
+	</div> 
+
+	<!--Start categorie!-->
+	<div class="category">
+
+		<div class="items">
+
+			<a class="items_cat" class="item_vraag" href="#"><h1>Ik heb een vraag</h1></a>
+			<a class="items_cat" class="item_discussie" href="#"><h1>Een discussie starten</h1></a>
+			<a class="items_cat" class="item_initiatief" href="#"><h1>Een initiatief</h1></a>
+			<a class="items_cat" class="item_aanmelden" href="#"><h1>Holtenbroek</h1></a> </br><p>Hier content </p><a class="btn_aanmelden" href="#">Aanmelden!</a>
+
+
+		</div>
+
+
+	</div>
+
+
+<!--Start nieuws uit de buurt-->
+
+	<div class="nieuws_buurt">
+
+		<div class="subject"><h1>Nieuws uit de buurt</h1></div>
+
+			<div class="nieuws_cat" class="nieuws_cat1" href="#"><h1>Onderwerp</h1><p>tekst</p><a class="btn_leesmeer" href="#">lees meer</a></div>
+			<div class="nieuws_cat" class="nieuws_cat2" href="#"><h1>Onderwerp</h1><p>tekst</p><a class="btn_leesmeer" href="#">lees meer</a></div>
+			<div class="nieuws_cat" class="nieuws_cat3" href="#"><h1>Onderwerp</h1><p>tekst</p><a class="btn_leesmeer" href="#">lees meer</a></div>
+			
+
+	</div>
+
+
+	<!-- Start Maak uw keuze submenu -->
+
+	<div class="sub_menu"> 
+
+
+
+			<ul> 
+					<li><div class="subject_submenu"><h1>Maak uw keuze</h1></div></li>
+					<li><a href="#" class="subject_name"><p>Plein</p></a></li>
+					<li><a href="#" class="subject_name"><p>Initiatieven</p></a></li>
+					<li><a href="#" class="subject_name"><p>Discussies</p></a></li>
+					<li><a href="#" class="subject_name"><p>Nieuwsbrief</p></a></li>
+					<li><a href="#" class="subject_name"><p>Media</p></a></li>
+					<li><a href="#" class="subject_name"><p>Misbruik melden</p></a></li>
+
+			</ul>
+
+
+	</div>
+
+
+	<!-- Start agenda -->
+
+	<div class="agenda">
+	
+	<div class="subject_agenda"><h1>Agenda</h1></div>
+
+
+
+	</div>
+
+
+<!-- Start Twitter API -->
+
+	<div class="twitter">
+
+
+
+
+	</div>
+
+
+
+
+
+
+<!-- einde wrapper -->
+
+</div>
+
+
+
+
+
+
+</body>
+
+
+</html>	
